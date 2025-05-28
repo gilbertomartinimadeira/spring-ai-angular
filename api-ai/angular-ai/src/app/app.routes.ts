82b09487c2d2a8c6cd435392c36eaf44ca9a019f
Gilberto Martini <gilberto.madeira@outlook.com>
2025-05-28 18:40:48 -0300

    added frontend project

---
+++ api-ai/angular-ai/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'simple-chat',
+        pathMatch: 'full'
+    },
+    {
+        path: 'simple-chat',
+        loadComponent: () => import('./chat/simple-chat/simple-chat')
+            .then(c => c.SimpleChat)
+    },
+    { path: '**', redirectTo: 'simple-chat' }
+];
---
+++ api-ai/angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-simple-chat',
+  imports: [MatCardModule],
+  templateUrl: './simple-chat.html',
+  styleUrl: './simple-chat.scss'
+})
+export class SimpleChat {
+
+}
