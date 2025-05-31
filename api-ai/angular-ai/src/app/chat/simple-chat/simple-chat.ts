634761085b544a623ce00472a2db47c34a75f4ae
Gilberto Martini <gilberto.madeira@outlook.com>
2025-05-31 13:38:51 -0300

    implements chat mock response

--- api-ai/angular-ai/src/app/chat/simple-chat/simple-chat.ts
+++ api-ai/angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-simple-chat',
@@ -12,16 +14,45 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    NgClass
   ],
   templateUrl: './simple-chat.html',
   styleUrl: './simple-chat.scss'
 })
 export class SimpleChat {
+
+  userPrompt = 'bla bla bla';
+
   messages = signal([
     {
       text: 'Hello, how can I help you today?',
       isBot: true
     }
   ]);
+
+  onSendMessage() {
+    this.trimUserPrompt();
+    if (this.userPrompt !== '') {
+      this.updateMessages(this.userPrompt)
+    }
+    this.userPrompt = '';
+    this.simulateResponse();
+  }
+  private trimUserPrompt() {
+    this.userPrompt = this.userPrompt.trim();
+  }
+
+  private simulateResponse() {
+    setTimeout(() => {
+      const response = 'This is a simulated response from Chat AI';
+      this.updateMessages(response, true);
+    }, 2000);
+
+  }
+
+  private updateMessages(text: string, isBot = false) {
+    this.messages.update(messages => [...messages, { text: text, isBot: isBot }]);
+  }
 }
